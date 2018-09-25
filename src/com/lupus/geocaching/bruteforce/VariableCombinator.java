6622fe0479cce782ca8dcbeee52a17b7763493b8
Michael Wolff <mlwolff@gmx.net>
2018-09-25 19:02:58 +0200

    Ongoing implementation

--- src/com/lupus/geocaching/bruteforce/VariableCombinator.java
+++ src/com/lupus/geocaching/bruteforce/VariableCombinator.java
@@ -8,7 +8,7 @@ import java.util.Objects;
 public class VariableCombinator<T extends Object> {
 	private final List<Variable<T>> variables;
 	
-	public VariableCombinator(Collection<Variable<T>> variables) {
+	public VariableCombinator(Collection<Variable<T>> variables, ValueSwitcher<T> switcher) {
 		Objects.requireNonNull(variables);
 		
 		this.variables = new ArrayList<>(variables);
--- src/com/lupus/geocaching/bruteforce/test/VarTest.java
+++ src/com/lupus/geocaching/bruteforce/test/VarTest.java
@@ -3,6 +3,8 @@ package com.lupus.geocaching.bruteforce.test;
 import java.util.Arrays;
 import java.util.List;
 
+import com.lupus.geocaching.bruteforce.DigitSwitcher;
+import com.lupus.geocaching.bruteforce.ValueSwitcher;
 import com.lupus.geocaching.bruteforce.Variable;
 import com.lupus.geocaching.bruteforce.VariableCombinator;
 
@@ -10,10 +12,10 @@ public class VarTest {
 
 	public static void main(String[] args) {
 		List<Variable<Integer>> vList = Arrays.asList(new Variable<>("A"), new Variable<>("B"));
+		ValueSwitcher<Integer> switcher = new DigitSwitcher();
 
-		 VariableCombinator<Integer> vc = new VariableCombinator<Integer>(vList);
-		 
-		 vc.printVariables();
+		VariableCombinator<Integer> vc = new VariableCombinator<Integer>(vList, switcher);
+
+		vc.printVariables();
 	}
-
 }
