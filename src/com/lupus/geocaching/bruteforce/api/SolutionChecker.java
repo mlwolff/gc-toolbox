e0d015a220711fd99fe977b2d508007d58680389
Michael Wolff <mlwolff@gmx.net>
2018-12-01 15:38:34 +0100

    Javadoc addded.

--- src/com/lupus/geocaching/bruteforce/api/SolutionChecker.java
+++ src/com/lupus/geocaching/bruteforce/api/SolutionChecker.java
@@ -11,9 +11,11 @@ import com.lupus.geocaching.bruteforce.VariableCombination;
 public interface SolutionChecker<T> {
     
     /**
-     * 
+     * Checks if the passed {@link VariableCombination} matches the solution requirements
      * @param combination
+     *      {@code VariableCombination} to check.
      * @return
+     *      <code>true</code>, if combination matches solution requirements.
      */
     boolean checkSolution(VariableCombination<T> combination);
 }
